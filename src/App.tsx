0e9cdba1917ec5f8674ff6ca1f9833f10ab6d6e5
Ivana <ivanacebova@gmail.com>
2023-09-04 10:00:20 +0200

    add first questions input with validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+/**
+ * Represents the possible gender values.
+ *
+ * @typedef {('M' | 'F' | 'Other')} GenderProps
+ * @description
+ * Use this type to define the gender of a person. It can be one of the following values:
+ * - 'M': Male
+ * - 'F': Female
+ * - 'Other': Other or unspecified gender
+ *
+ * @example
+ * // Usage example:
+ * const userGender: GenderProps = 'M';
+ */
+type GenderProps = 'M' | 'F' | 'Other';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [age, setAge] = useState<{ ageValue: number } | null>(null);
+    const [gender, setGender] = useState<GenderProps | ''>('');
+    const [hasLicense, setHasLicense] = useState<string | null>(null);
+    const [isFirstCar, setIsFirstCar] = useState<string | null>(null);
+    const [surveyComplete, setSurveyComplete] = useState<boolean>(false);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Validation
+        if (age !== null && age.ageValue < 18) {
+            alert('Thank you for your interest, but you are under 18.');
+        } else if (hasLicense === 'No') {
+            alert('Thank you for your interest, but you prefer using other transport.');
+        } else if (age !== null && age.ageValue >= 18 && age.ageValue <= 25 && isFirstCar === 'Yes') {
+            alert('We are targeting more experienced clients. Thank you for your interest.');
+        } else {
+            alert('Thank you for completing the survey!');
+            setSurveyComplete(true);
+        }
+    };
+
+
+    const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const ageValue = parseInt(e.target.value);
+        if (!isNaN(ageValue)) {
+            setAge({ageValue: ageValue});
+        }
+    };
+
+    const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedValue = e.target.value;
+
+        if (selectedValue === 'M' || selectedValue === 'F' || selectedValue === 'Other') {
+            setGender(selectedValue as GenderProps);
+        } else {
+            // Handle the case where the selected value is not one of the specified values.
+            console.error('Invalid gender selected:', selectedValue);
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Car Sales Survey</h1>
+            {!surveyComplete && (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="age">Age:</label>
+                    <input
+                        type="number"
+                        id="age"
+                        value={age?.ageValue || ''}
+                        onChange={handleAgeChange}
+                        required
+                    />
+                    <br/>
+
+                    <label htmlFor="gender">Gender:</label>
+                    <select
+                        id="gender"
+                        value={gender}
+                        onChange={handleGenderChange}
+                        required
+                    >
+                        <option value="">Select</option>
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                    <br/>
+
+                    <label htmlFor="license">Do you own a car driving license?</label>
+                    <input
+                        type="radio"
+                        id="yes-license"
+                        name="license"
+                        value="Yes"
+                        checked={hasLicense === 'Yes'}
+                        onChange={() => setHasLicense('Yes')}
+                        required
+                    />
+                    <label htmlFor="yes-license">Yes</label>
+                    <input
+                        type="radio"
+                        id="no-license"
+                        name="license"
+                        value="No"
+                        checked={hasLicense === 'No'}
+                        onChange={() => setHasLicense('No')}
+                        required
+                    />
+                    <label htmlFor="no-license">No, I prefer using other transport</label>
+                    <br/>
+
+                    {/* Bonus question for users aged 18-25 */}
+                    {age !== null && age.ageValue >= 18 && age.ageValue <= 25 && (
+                        <div>
+                            <label htmlFor="first-car">Is this your first car?</label>
+                            <input
+                                type="radio"
+                                id="yes-first-car"
+                                name="first-car"
+                                value="Yes"
+                                checked={isFirstCar === 'Yes'}
+                                onChange={() => setIsFirstCar('Yes')}
+                            />
+                            <label htmlFor="yes-first-car">Yes</label>
+                            <input
+                                type="radio"
+                                id="no-first-car"
+                                name="first-car"
+                                value="No"
+                                checked={isFirstCar === 'No'}
+                                onChange={() => setIsFirstCar('No')}
+                            />
+                            <label htmlFor="no-first-car">No</label>
+                        </div>
+                    )}
+
+                    <button type="submit">Submit</button>
+                </form>
+            )}
+
+            {surveyComplete && (
+                <div>
+                    <p>Thank you for completing the survey!</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
